45ec60e6bf32fc928b6161e1ce43b071a28add47
David Spiessens <david.spiessens@pelckmans.be>
2026-08-12 09:48:32 +0200

    wip

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
-/home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/bax.js b-stock && /home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/archive_products.js 1
-/home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/aedsecondhand.js && /home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/archive_products.js 5
-/home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/xlrpro.js && /home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/archive_soldout_xlrpro.js 4
-/home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/soundsale.js && /home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/archive_products.js 6
-/home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/progear.js && /home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/archive_products.js 3
-/home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/cuesale.js && /home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/archive_products.js 11
-/home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/salesall.js && /home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/archive_products.js 10
-/home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/kinxsound.js && /home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/archive_products.js 12
-/home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/coolblue.js && /home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/archive_products.js 13
-/home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/brands.js
-/home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/link_brands.js
-/home/david/.nvm/versions/node/v24.19.0/bin/node /home/david/slscraper/link_products.js
+#!/bin/bash
+
+NODE="/home/david/.nvm/versions/node/v24.19.0/bin/node"
+DIR="/home/david/slscraper"
+
+$NODE $DIR/bax.js b-stock && $NODE $DIR/archive_products.js 1
+$NODE $DIR/aedsecondhand.js && $NODE $DIR/archive_products.js 5
+$NODE $DIR/xlrpro.js && $NODE $DIR/archive_soldout_xlrpro.js 4
+$NODE $DIR/soundsale.js && $NODE $DIR/archive_products.js 6
+$NODE $DIR/progear.js && $NODE $DIR/archive_products.js 3
+$NODE $DIR/cuesale.js && $NODE $DIR/archive_products.js 11
+$NODE $DIR/salesall.js && $NODE $DIR/archive_products.js 10
+$NODE $DIR/kinxsound.js && $NODE $DIR/archive_products.js 12
+$NODE $DIR/coolblue.js && $NODE $DIR/archive_products.js 13
+$NODE $DIR/brands.js
+$NODE $DIR/link_brands.js
+$NODE $DIR/link_products.js
