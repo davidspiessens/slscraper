d566247f8654aaa7b326a865711652f6f9878794
David Spiessens <david.spiessens@pelckmans.be>
2026-08-13 16:01:08 +0200

    wip

--- run.sh
+++ run.sh
@@ -48,7 +48,7 @@ $NODE $DIR/thomann.js && $NODE $DIR/archive_products.js 14
 $NODE $DIR/brands.js
 $NODE $DIR/link_brands.js
 $NODE $DIR/link_products.js
-$NODE $DIR/musicstore.js && $NODE $DIR/archive_products.js 15
-$NODE $DIR/brands.js
-$NODE $DIR/link_brands.js
-$NODE $DIR/link_products.js
+# $NODE $DIR/musicstore.js && $NODE $DIR/archive_products.js 15
+# $NODE $DIR/brands.js
+# $NODE $DIR/link_brands.js
+# $NODE $DIR/link_products.js
